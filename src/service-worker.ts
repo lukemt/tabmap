b87589a616b2540b2bef35be8c14eb53e4e19833
Lukas Petry <git.luke@rennwurst.de>
2023-07-23 19:02:52 +0200

    :construction_worker: Use Vite Plugin Web Extension

---
+++ src/service-worker.ts
@@ -0,0 +1,34 @@
+import browser from "webextension-polyfill";
+
+const openApp = async () => {
+  const appUrl = browser.runtime.getURL("index.html");
+  // query for existing app tab
+  const tabs = await browser.tabs.query({ url: appUrl });
+  if (tabs.length > 0) {
+    // if app tab exists, focus it
+    await browser.tabs.update(tabs[0].id, { active: true });
+    await browser.windows.update(tabs[0].windowId, { focused: true });
+  } else {
+    // if app tab does not exist, create it, pin it, and focus it
+    await browser.tabs.create({
+      url: appUrl,
+      pinned: true,
+      active: true,
+    });
+  }
+};
+
+// listen for click on extension icon
+browser.action.onClicked.addListener(openApp);
+
+// listen for keyboard shortcut
+browser.commands.onCommand.addListener((command) => {
+  console.log(`Command: ${command}`);
+  if (command === "open-app") {
+    openApp();
+  }
+});
+
+browser.runtime.onInstalled.addListener(() => {
+  console.log("Installed!");
+});
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import webExtension from "vite-plugin-web-extension";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    webExtension({
+      webExtConfig: {
+        startUrl: ["https://example.com/", "chrome://extensions/shortcuts"],
+      },
+    }),
+  ],
+});
