bdda8673bf992b718a33a94c72700731636e3312
Lukas Petry <git.luke@rennwurst.de>
2023-07-23 15:31:17 +0200

    :bug: Make openApp more robust

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,12 +1,22 @@
 let appTab = null;
 
-chrome.action.onClicked.addListener(async (tab) => {
-  try {
-    await chrome.tabs.update(appTab.id, { active: true });
-    await chrome.windows.update(appTab.windowId, { focused: true });
-  } catch (e) {
+const openApp = async (tab) => {
+  const appUrl = chrome.runtime.getURL("appDist/index.html");
+  // query for existing app tab
+  const tabs = await chrome.tabs.query({ url: appUrl });
+  if (tabs.length > 0) {
+    // if app tab exists, focus it
+    await chrome.tabs.update(tabs[0].id, { active: true });
+    await chrome.windows.update(tabs[0].windowId, { focused: true });
+  } else {
+    // if app tab does not exist, create it, pin it, and focus it
     appTab = await chrome.tabs.create({
-      url: chrome.runtime.getURL("appDist/index.html"),
+      url: appUrl,
+      pinned: true,
+      active: true,
     });
   }
-});
+};
+
+// listen for click on extension icon
+chrome.action.onClicked.addListener(openApp);
