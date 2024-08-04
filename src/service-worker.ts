d55b872e6952e58d685a7356e75865849ae49ac0
lukemt <lukemt.luke@rennwurst.de>
2024-08-04 17:45:27 +0200

    :fire: Remove opening tabs on startup feature

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -25,76 +25,15 @@ async function openApp() {
       url: appUrl,
       active: true,
       index: 0,
+      pinned: true,
     });
   }
 }
 
 async function onStartup() {
   console.log("onStartup");
-  // const tabMapData: TabInfoWithoutFavIconUrl[] =
-  //   (await browser.storage.local.get("tabMapData")) || [];
-  // setData(tabMapData);
-
-  const openTabs = getAllOpenTabs();
-
-  for (const tabInfo of openTabs) {
-    if (tabInfo.status === "openFront") {
-      await openTab(tabInfo);
-    } else if (tabInfo.status === "openBack") {
-      const tab = await openTab(tabInfo);
-      moveToGroup(tab);
-    }
-  }
-}
-
-async function openTab(tabInfo: PageWithoutFavIconUrl) {
-  console.log("openTab", { tabInfo });
-  const existingTabs = await browser.tabs.query({ url: tabInfo.url });
-  if (existingTabs.length > 0) {
-    return existingTabs[0];
-  }
-  return await browser.tabs.create({
-    url: tabInfo.url,
-    active: false,
-  });
-}
-
-async function moveToGroup(tab: any) {
-  console.log("moveToGroup", { tab });
-  // get group id
-  const groupId = await getTabGroup();
-  if (groupId !== null) {
-    // move tab to group
-    await chrome.tabs.group({
-      tabIds: [tab.id],
-      groupId,
-    });
-  } else {
-    // create group
-    console.log("create group");
-    const groupId = await chrome.tabs.group({
-      tabIds: [tab.id],
-    });
-    // move group to index 1
-    console.log("move group to index 1");
-    await chrome.tabGroups.move(groupId, { index: 1 });
-    // collapse group
-    console.log("collapse group");
-    await chrome.tabGroups.update(groupId, {
-      collapsed: true,
-    });
-  }
-}
-
-async function getTabGroup() {
-  console.log("getTabGroup");
-  const groups = await chrome.tabGroups.query({});
-  if (groups.length > 0) {
-    return groups[0].id;
-  } else {
-    console.log("no group found");
-    return null;
-  }
+  // nothing to do i guess
+  // maybe openApp()?
 }
 
 async function activateCommand() {
